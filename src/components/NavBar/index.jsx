12bde8cf4d94c157ff3a8bf50d4caca0543bdd99
cjc <431909623@qq.com>
2023-08-10 20:49:42 +0800

    fix: 完成底部导航栏

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,21 +1,48 @@
 import React from "react";
 import {TabBar } from 'antd-mobile'
 import { useNavigate } from "react-router-dom";
+import PropTypes from "prop-types"
+import {BillOutline, PieOutline, UserOutline } from 'antd-mobile-icons'
+import s from './style.module.less'
 
 const NavBar = function({showNav}) {
   const navigate = useNavigate();
   const changeTab = function(path) {
     navigate(path)
   }
+  // const items = [
+  //   {
+  //     key: '/',
+  //     title: '首页',
+  //     icon: <BillOutline />
+  //   },
+  //   {
+  //     key: '/data',
+  //     title: '统计',
+  //     icon: <PieOutline />
+  //   },
+  //   {
+  //     key: '/user',
+  //     title: '我的',
+  //     icon: <UserOutline />
+  //   }
+  // ]
   
 
-  return <div>
+  return <div className={s.NavBar}>
     <TabBar onChange={(activeKey) => changeTab(activeKey)}>
-      <TabBar.Item key="/" title="首页" />
-      <TabBar.Item key="/data" title="统计" />
-      <TabBar.Item key="/user" title="我的" />
+      {/* {items.map((item) => {
+        <TabBar.Item key={item.key} title={item.title} icon={item.icon} />
+      })} */}
+      <TabBar.Item key="/" title="首页" icon={<BillOutline />} />
+      <TabBar.Item key="/data" title="统计" icon={<PieOutline />} />
+      <TabBar.Item key="/user" title="我的" icon={<UserOutline />} />
     </TabBar>
-  </div>         
+  </div>    
+}
+
+NavBar.PropTypes = {
+  showNav: PropTypes.bool
 }
 
 export default NavBar
--- src/container/Index/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Button } from 'antd-mobile'
-import indexModule from './style.module.less'
-
-export default function Index() {
-  return <div className={indexModule.index}>
-    <span>Index</span><Button color='primary'>Primary</Button>
-  </div>
-}
